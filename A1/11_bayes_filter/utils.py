bda13dee38b3fd9923171f4bd89897e46f415962
martagonzalezjuan <marta.gonzalez11@estudiant.uib.cat>
2026-03-12 16:48:58 +0100

    bayes filter completo

--- A1/11_bayes_filter/utils.py
+++ A1/11_bayes_filter/utils.py
@@ -25,21 +25,68 @@ def plot_belief(belief):
     
 
 def motion_model(action, belief):
-    
-    # YOUR CODE HERE
-    raise NotImplementedError()
-    # -----
+    n_cells = belief.shape[0]
+    new_belief = np.zeros_like(belief)
+
+    if action == 'F':
+        correct_step = 1
+        opposite_step = -1
+    elif action == 'B':
+        correct_step = -1
+        opposite_step = 1
+    else:
+        raise ValueError("Action must be 'F' or 'B'.")
+
+    for x_prev, prob in enumerate(belief):
+        x_correct = x_prev + correct_step
+        x_opposite = x_prev + opposite_step
+
+        if 0 <= x_correct < n_cells:
+            new_belief[x_correct] += 0.7 * prob
+        else:
+            new_belief[x_prev] += 0.7 * prob
+
+        new_belief[x_prev] += 0.2 * prob
+
+        if 0 <= x_opposite < n_cells:
+            new_belief[x_opposite] += 0.1 * prob
+        else:
+            new_belief[x_prev] += 0.1 * prob
+
+    return new_belief
   
     
 def sensor_model(observation, belief, world):
+    world = np.asarray(world).astype(int)
 
-    # YOUR CODE HERE
-    raise NotImplementedError()
-    # -----
+    if observation not in (0, 1):
+        raise ValueError("Observation must be 0 (black) or 1 (white).")
+
+    if observation == 1:
+        likelihood = np.where(world == 1, 0.7, 0.1)
+    else:
+        likelihood = np.where(world == 0, 0.9, 0.3)
+
+    updated_belief = belief * likelihood
+    normalizer = np.sum(updated_belief)
+
+    if normalizer <= 0.0:
+        raise ValueError("Belief normalization failed: zero total probability.")
+
+    return updated_belief / normalizer
 
 
 def recursive_bayes_filter(actions, observations, belief, world):
 
-    # YOUR CODE HERE
-    raise NotImplementedError()
-    # -----
+    n_actions = len(actions)
+    n_observations = len(observations)
+
+    current_belief = belief.copy()
+
+    if n_observations == n_actions + 1:
+        current_belief = sensor_model(observations[0], current_belief, world)
+        for action, observation in zip(actions, observations[1:]):
+            current_belief = motion_model(action, current_belief)
+            current_belief = sensor_model(observation, current_belief, world)
+
+    return current_belief
